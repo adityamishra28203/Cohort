f22674264fe43f6cc0496706e1b83cd49f2784b7
adityamishra28203 <adityamishraubi@gmail.com>
2024-10-13 22:24:55 +0530

    fix the error

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react' //hook- {useState} to define state
 
 function App() {
-  const [count, setCount] = useState(0)    // count is a variable and setCount is a function which is re-rendering the components 
+  const [count, setCount] = useState(0);    // count is a variable and setCount is a function which is re-rendering the components 
   return (
     <div>
-      <customButton count={count} setCount={setCount}></customButton>
+      <CustomButton count={count} setCount={setCount}></CustomButton>
     </div>
   )
-};
+}
 
 //button component
-function customButton (props) {
+function CustomButton(props) {  //components to be rendered in React, it should start will capital letter
+
   function onClickHandler() {
-    props.setCount(count + 1);
+    props.setCount(props.count + 1);
   }
+
   return <button onClick={onClickHandler}>
     Counter {props.count}
   </button>
