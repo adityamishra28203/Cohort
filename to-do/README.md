6afaec90ed4a8741d93acb807b1552841f1622d0
adityamishra28203 <adityamishraubi@gmail.com>
2024-10-20 23:03:22 +0530

    todo app

---
+++ to-do/README.md
@@ -0,0 +1,10 @@
+//md stands for mark down, mark down is used to write pretty things
+## TODO App
+This project contains a simple TODO application.
+It has the following features:
+- Anyone can create a Todo
+- Anyone can see their existing todos
+- Anyone can mark a todo as done
+
+//initialise a node project
+//put a package.json 
---
+++ to-do/backend/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const { createTodo, updateTodo } = require('./types')
+const app = express();
+
+app.use(express.json());
+
+
+app.post("/todo", function (req, res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    
+    if (!parsedPayload.success){
+        res.status(11).json({
+            msg: "Invalid inputs"
+        });
+        return;
+    }
+    //put in MongoDB
+})
+
+app.get("/todos", function (req, res) {
+
+})
+
+
+app.put("/completed", function (req, res) {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "Invalid todo"
+        });
+        return;
+    }
+    //updating in mongodb
+})
