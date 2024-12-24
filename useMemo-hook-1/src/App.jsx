028d2f8deb64fec3a0984374224ff5e08c54876e
adityamishra28203 <adityamishraubi@gmail.com>
2024-12-25 03:48:13 +0530

    simple sum app optmized using useMemo

--- useMemo-hook-1/src/App.jsx
+++ useMemo-hook-1/src/App.jsx
@@ -1,13 +1,24 @@
+import { useMemo } from 'react'
 import { useState } from 'react'
 
 function App() {
   const [inputValue, setInputValue] = useState(1)
   const [counter, setCounter] = useState(0)
 
-  let sum = 0
-  for(let i=1; i <= inputValue; i++) {
-    sum+=i;
-  }
+  //this is rendering everytime event the sum value is not changing because of counter value is changing
+  // let sum = 0
+  // for(let i=1; i <= inputValue; i++) {
+  //   sum+=i;
+  // }
+
+  //we will use useMemo hook to memorize the sum value for all the re-rendering until the inputValue is changed
+
+  let sum = useMemo( () => {
+    let finalSum = 0
+    for( let i=1; i <= inputValue; i++ ) {
+      finalSum += i;
+    }
+  }, [inputValue])
 
   return <div>
     <input onChange={ function (e) { setInputValue(e.target.value)}} placeholder='Find sum from 1 to n'></input> 
