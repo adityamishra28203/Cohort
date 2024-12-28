4f1a62d50d7c66cd25a916b60b450ad78463022f
adityamishra28203 <adityamishraubi@gmail.com>
2024-12-29 00:02:47 +0530

    recoil-usecase code addded

--- recoil-usecase/src/App.jsx
+++ recoil-usecase/src/App.jsx
@@ -1,5 +1,5 @@
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
-import { countAtom } from "./store/atoms/count";
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { countAtom, evenSelector } from "./store/atoms/count";
 
 function App () {
 return (
@@ -8,13 +8,13 @@ return (
       <Count />
     </RecoilRoot>
   </div>
-)
-}
+)}
 
 function Count () {
   return <div>
     <CountRenderer />
     <Buttons />
+    <EvenCountRenderer />
   </div>
 }
 
@@ -28,16 +28,25 @@ function CountRenderer () {
 }
 
 function Buttons () {
-  const [count, setCount] = useRecoilState(countAtom);        //here, we require both count value and state updation
+  //const [count, setCount] = useRecoilState(countAtom);        //here, we require both count value and state updation
+
+  const setCount = useSetRecoilState(countAtom)       //we actually don't need count, which was causing button component re-rendering when clicked
   return <div>
     <button onClick={() => {
-      setCount(count+1)
+      setCount(count => count + 1)                     // here, we donot use count directly instead using func. to update the state with local var. count
     }}>Increase</button>
     
     <button onClick={() => {
-      setCount(count-1)
+      setCount(count => count - 1)
     }}>Decrease</button>
 
+  </div>
+}
+
+function EvenCountRenderer () {
+  const isEven = useRecoilValue(evenSelector)            
+  return <div>
+    {isEven ? "It is Even" : null}
   </div>
 }
 
--- recoil-usecase/src/store/atoms/count.jsx
+++ recoil-usecase/src/store/atoms/count.jsx
@@ -1,9 +1,18 @@
-import { atom } from 'recoil'
+import { atom, selector } from 'recoil'
 
 export const countAtom = atom({
     key: "countAtom",
     default: 0
 });
-
-
 //defining atom
+
+export const evenSelector = selector({
+    key: "evenSelector",
+    get: ({get}) => {
+        const count = get(countAtom)         //depends on count, when count valure changes it will give the output as either true or false
+        return count%2 == 0; 
+    }
+})
+
+//defining selector
+//selector: A selector represents a piece of derived state. Similar to useMemmo, which gives the data when the dependency is changed
